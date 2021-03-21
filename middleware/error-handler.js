8ab185a61c2810e2adb5b7de1c3506bd1ae47b41
Adrian Tang <adriantang.job@gmail.com>
2021-03-21 19:20:27 +0000

    Handle malformed request body error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -3,6 +3,8 @@ const BaseError = require('../errors/base-error');
 const errorHandler = (err, req, res, next) => {
     if (err instanceof BaseError) {
         res.status(err.statusCode).send({ errors: err.serialize() });
+    } else if (err instanceof SyntaxError) {
+        res.status(err.statusCode).send({ errors: [{ message: 'Malformed request body' }] });
     } else {
         // Unexpected error encountered
 
